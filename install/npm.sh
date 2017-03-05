94707de5d647e776a899ef6806ba626f8ed58db3
Jon Milner <email@jonmilner.com>
2017-03-05 18:49:12 -0500

    Update npm.sh

--- install/npm.sh
+++ install/npm.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-if test ! $(which nvm)
-then
-  echo "Installing a stable version of Node..."
-
-  # Install the latest stable version of node
-  nvm install stable
-
-  # Switch to the installed version
-  nvm use node
-
-  # Use the stable version of node by default
-  nvm alias default node
-fi
-
-# All `npm install <pkg>` commands will pin to the version that was available at the time you run the command
-npm config set save-exact = true
+#Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 # Globally install packages with npm
 npm install -g diff-so-fancy
