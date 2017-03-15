5dc387cf8a1b8a66601226bd1ef16de6d6940ded
Jon Milner <jonmilner@users.noreply.github.com>
2017-03-15 19:51:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,6 +14,9 @@ done
 # Symlinks
 ###############################################################################
 
+# Atom
+ln -sfn /Applications/Atom.app/Contents/Resources/app/atom.sh /usr/local/bin/atom
+
 # Git
 ln -sfn dotfiles/git/gitattributes ~/.gitattributes
 ln -sfn dotfiles/git/gitconfig ~/.gitconfig
