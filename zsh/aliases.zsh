0690674be9bd796615fe27775fa916f1b6bc5430
Jon Milner <email@jonmilner.com>
2017-04-05 18:04:37 -0400

    Add git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,12 @@
 alias c="clear"
 alias ll="ls -lFh"
 alias la="ls -lAFh"  # List all files (inlcuding hidden)
+
+# git
+alias ga="git add"
+alias gcm="git commit -m"
+alias gd="git diff"
+alias gl="git l"
+alias gp="git pull"
+alias gpu="git push"
+alias gs="git status"
