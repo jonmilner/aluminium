177b828d24580def6444615ef04efd36bd1e007b
Jon Milner <email@jonmilner.com>
2019-03-12 02:09:36 -0400

    Updates

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -2,16 +2,29 @@ alias c="clear"
 alias ll="ls -lFh"
 alias la="ls -lAFh"  # List all files (inlcuding hidden)
 
-# finder
-alias open="open -a ForkLift"
-
 # git
-alias co="checkout"
-alias ga="git add"
-alias gco="git co"
-alias gcm="git commit -m"
+
+## branch
+alias gb="git checkout -b" # branch & checkout
+alias gbl="git branch --sort=-committerdate" # list by date
+
+## checkout
+alias co="git checkout"
+alias gco="git checkout"
+
+## status
 alias gd="git diff"
 alias gl="git l"
+alias gs="git status"
+
+## push/pull
 alias gp="git pull"
 alias gpu="git push"
-alias gs="git status"
+
+# add/commit
+alias ga="git add"
+alias gcm="git commit -m"
+
+# stash
+alias gsp="git stash pop"
+alias gst="git stash save -u"
