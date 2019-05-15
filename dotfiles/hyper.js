07c8a40bb544fb577a72a44cbe439bdda8a238ba
Jon Milner <email@jonmilner.com>
2019-05-15 14:07:36 -0400

    add hyper plugins

--- dotfiles/hyper.js
+++ dotfiles/hyper.js
@@ -8,79 +8,87 @@ module.exports = {
     uiFontFamily: '"Operator Mono", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: "rgba(248,28,229,0.8)",
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
+    cursorShape: "BLOCK",
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: "#fff",
 
     // terminal background color
-    backgroundColor: '#000',
+    backgroundColor: "#112827",
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: "#333",
 
     // custom css to embed in the main window
     css: `
       * {
         letter-spacing: 0.02em;
       }
+
+      .splitpane_divider {
+        background-color: #000 !important;
+      }
+
+      .splitpane_pane > .term_fit > div:first-child {
+        display: none !important;
+      }
     `,
 
     // custom css to embed in the terminal window
-    termCSS: '',
+    termCSS: "",
 
     // set to `true` if you're using a Linux set up
     // that doesn't shows native menus
     // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
+    showHamburgerMenu: "",
 
     // set to `false` if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
+    showWindowControls: "",
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '0.75rem 1rem',
+    padding: "0.75rem 1rem",
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      gray: '#808080',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
+      black: "#000000",
+      red: "#ff0000",
+      green: "#33ff00",
+      yellow: "#ffff00",
+      blue: "#0066ff",
+      magenta: "#cc00ff",
+      cyan: "#00ffff",
+      white: "#d0d0d0",
+      gray: "#808080",
+      lightBlack: "#808080",
+      lightRed: "#ff0000",
+      lightGreen: "#33ff00",
+      lightYellow: "#ffff00",
+      lightBlue: "#0066ff",
+      lightMagenta: "#cc00ff",
+      lightCyan: "#00ffff",
+      lightWhite: "#ffffff"
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
-    shell: 'zsh',
+    shell: "zsh",
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
-    shellArgs: ['--login'],
+    shellArgs: ["--login"],
 
     // for environment variables
     env: {},
 
     // set to false for no bell
-    bell: 'SOUND',
+    bell: "SOUND",
 
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: false
@@ -98,7 +106,11 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    'hyperterm-atom-dark'
+    "hyperterm-atom-dark",
+    "hyper-opacity",
+    "hyperpower",
+    "hyper-highlight-active-pane",
+    "hyper-pane"
   ],
 
   // in development, you can create a directory under
