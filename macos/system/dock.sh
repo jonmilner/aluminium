b2128f215bfbf9f09a080b84ef39fc3fd1c22680
Jon Milner <jonmilner@users.noreply.github.com>
2020-12-18 19:32:07 -0500

    Don't show recent applications in Dock

--- macos/system/dock.sh
+++ macos/system/dock.sh
@@ -22,6 +22,9 @@ sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.a
 echo "∙ Remove default apps from the Dock"
 defaults write com.apple.dock persistent-apps -array
 
+echo "∙ Don't show recent applications in Dock"
+defaults write com.apple.dock show-recents -bool FALSE
+
 echo "∙ Add Applications to Dock"
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Dropbox.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
