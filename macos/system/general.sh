f60458a316ae9a53c80a902c0b2191001d68156b
Jon Milner <jonmilner@users.noreply.github.com>
2020-12-18 19:49:36 -0500

    Update general.sh

--- macos/system/general.sh
+++ macos/system/general.sh
@@ -4,15 +4,5 @@
 echo "General"
 ###############################################################################
 
-echo "∙ Show battery percentage in menu bar"
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
-echo "∙ Use Dark menu bar and Dock"
-defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
-
 echo "∙ Automatically hide and show the menu bar"
 defaults write NSGlobalDomain "_HIHideMenuBar" -bool true
-
-echo "∙ Scroll bar visibility"
-# Possible values: `WhenScrolling`, `Automatic` and `Always`
-defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
